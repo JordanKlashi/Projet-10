29b56abff15f64ebc341f00e38dd3cfb704fdaff
Verbreuk Jordan <jordan-010@hotmail.com>
2024-04-11 15:48:58 +0200

    update
    update deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-/* istanbul ignore file */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Page from "./pages/Home";
 import { DataProvider } from "./contexts/DataContext";
 
 function App() {
   return (
-    <DataProvider>
-      <Page />
-    </DataProvider>
+    <Router>
+      <Routes>
+        <DataProvider>
+          <Page />
+          <Route path="/Projet-10" element={<Page />} />
+        </DataProvider>
+      </Routes>
+    </Router>
   );
 }
 
